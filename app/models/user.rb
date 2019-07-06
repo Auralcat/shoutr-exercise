95e039a70cb66f40d17eb16a28d61fe544be724d
Auralcat <ashleyzt@hi2.in>
2019-07-06 15:14:32 -0300

    Extract dashboard and timeline entity from User model
    Previously, the User model had the responsibility to process the Shouts
    to show in the main view, and that didn't seem too well for single
    responsibility principles. This also removes the dangerous
    `default_scope` method in the User model and makes that logic clearer in
    another object.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,21 +9,17 @@ class User < ApplicationRecord
 
   has_many :followed_user_relationships,
     foreign_key: :follower_id,
-    class_name: "FollowingRelationship",
+    class_name: 'FollowingRelationship',
     dependent: :destroy
 
   has_many :followed_users, through: :followed_user_relationships
 
   has_many :follower_relationships,
     foreign_key: :followed_user_id,
-    class_name: "FollowingRelationship",
+    class_name: 'FollowingRelationship',
     dependent: :destroy
 
   has_many :followers, through: :follower_relationships
-
-  def timeline_shouts
-    Shout.where(user_id: followed_user_ids + [id])
-  end
 
   def like(shout)
     liked_shouts << shout
